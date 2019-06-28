532a2327239abe0b6ed3c9ab987d62e4c6d5d0c0
Ashish Patil <ashishnitinpatil@gmail.com>
2019-06-29 00:25:17 +0400

    ~upgrade solidity to 0.5.8

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.8;
 
 // It's important to avoid vulnerabilities due to numeric overflow bugs
 // OpenZeppelin's SafeMath library, when used correctly, protects agains such bugs
